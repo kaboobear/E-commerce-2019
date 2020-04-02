28170e764130afe93e6ecbbbeeb43dd80fc0be81
Kaboo <kaboo.bear@gmail.com>
2020-04-03 01:06:22 +0300

    'second'

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use('/order',order_route);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
 }
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+})
 
 app.listen(port,()=>{console.log(`Server started on port ${port}`)}) 
